ddba2759c9cefddc6d1a08a2f4f34c002999ef9e
Zachary LOBATO <zacharyisaiahlobato@hotmail.com>
2016-07-14 17:39:03 -0600

    initial commit

---
+++ FridayFun!/Cuts.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FridayFun_
+{
+    class Cuts
+    {
+        public int minimumNumberOfCuts (int stickLength) {
+
+            int cutsCount = 0;
+            
+            // If stick is of length one it can't be cut
+            if (stickLength <= 1)
+            {
+                return cutsCount;
+            }
+
+            cutsCount = 1;
+            int potentialPieces = 2;
+
+            // Until the stick is of the correct length, double the number of potential pieces
+            while (potentialPieces < stickLength)
+            {
+                potentialPieces *= 2;
+                cutsCount++;
+            }
+
+            return cutsCount;
+        }
+
+    }
+}
---
+++ FridayFun!/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FridayFun_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cuts test = new Cuts();
+
+            for (int i = 0; i<=100; i++){
+                Console.Out.WriteLine(i + " " + test.minimumNumberOfCuts(i));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
